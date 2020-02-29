ff94c398765fccd243bf12a5d7749f8d6781e3dc
Tyla Devon <tyladevon@gmail.com>
2020-02-29 13:34:54 -0700

    Add forecast controller with show action

---
+++ app/controllers/api/v1/forecast_controller.rb
@@ -0,0 +1,12 @@
+class Api::V1::ForecastController < ApplicationController 
+  def show
+    forecast = ForecastFacade.new(location_params).forecast
+    ForecastSerializer.new(forecast)
+  end 
+
+  private 
+
+  def location_params
+    params[:location]
+  end
+end
