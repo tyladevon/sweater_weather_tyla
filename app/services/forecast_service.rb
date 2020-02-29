e4b9380e592440c3bf9cc9bdc1408045e343a3c9
Tyla Devon <tyladevon@gmail.com>
2020-02-29 14:26:51 -0700

    Refactor results for symbilzed names and create private method

--- app/services/forecast_service.rb
+++ app/services/forecast_service.rb
@@ -1,10 +1,18 @@
 class ForecastService
   def initialize(location)
-    @lat = location["results"].first["geometry"]["location"]["lat"]
-    @lon = location["results"].first["geometry"]["location"]["lng"]
+    @lat = location[:results].first[:geometry][:location][:lat]
+    @lon = location[:results].first[:geometry][:location][:lng]
   end
   
   def forecast
+    conn 
+  end 
 
+  private
+
+  def conn
+    response = Faraday.get("https://api.darksky.net/forecast/#{ENV['DARKSKY_API']}/#{@lat},#{@lon}") do |req|
+    end
+    JSON.parse(response.body, symbolize_names: true)
   end 
 end 
