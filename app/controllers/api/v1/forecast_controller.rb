b6c9f1fe8397a17518d3436280b61b378efde061
Tyla Devon <tyladevon@gmail.com>
2020-02-29 14:23:53 -0700

    Update Forecast Controller for rendering json

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::ForecastController < ApplicationController 
   def show
     forecast = ForecastFacade.new(location_params).forecast
-    ForecastSerializer.new(forecast)
+    render json: ForecastSerializer.new(forecast)
   end 
 
   private 
