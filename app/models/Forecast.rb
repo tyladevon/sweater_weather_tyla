827ce4925f249bf3d1353c175194829c432b4436
Tyla Devon <tyladevon@gmail.com>
2020-02-29 22:25:21 -0700

    Continue work on Forecast poro for methods to display formatted data.

--- app/models/Forecast.rb
+++ app/models/Forecast.rb
@@ -7,14 +7,22 @@ class Forecast
   def current
     binding.pry
     current_info = {
-      summary: data[:currently][:summary],
-      icon: data[:currently][:icon],
-      temperature: data[:currently][:temperature]
+      summary: data[:daily][:summary],
+      icon: data[:daily][:icon],
+      temperature: data[:currently][:temperature],
+      high: data[:daily][:data][:temperatureHigh],
+      low: data[:daily][:data][:temperatureLow],
+      location: ,
+      time: 
     }
   end 
 
   def details
+    detail_info = {
+      summary: ,
+      icon: ,
 
+    }
   end 
 
   def forecast
