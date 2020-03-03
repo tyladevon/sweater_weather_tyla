e02c9df9171ab8f80cc8489f516ab63e8ece30fd
Tyla Devon <tyladevon@gmail.com>
2020-03-03 11:17:15 -0700

    Finish sad path testing for create user

--- spec/requests/api/v1/can_register_via_post_request_spec.rb
+++ spec/requests/api/v1/can_register_via_post_request_spec.rb
@@ -11,8 +11,9 @@ describe "users endpoint" do
     post '/api/v1/users', params: params
 
     expect(response).to be_successful
+    result = JSON.parse(response.body, symbolize_names: true)
+    expect(result[:data][:attributes]).to_not be_empty
 
-    JSON.parse(response.body, symbolize_names: true)
   end
 
   it "cannot register if passwords are mismatched" do
@@ -24,8 +25,9 @@ describe "users endpoint" do
 
     post '/api/v1/users', params: params
 
+    result = JSON.parse(response.body, symbolize_names: true)
     expect(response.status).to eq(400)
-    expect(response.body).to_not be_empty
+    expect(result[:error]).to_not be_empty
   end
 
   it "cannot register if email is missing" do
@@ -37,8 +39,9 @@ describe "users endpoint" do
 
     post '/api/v1/users', params: params
 
+    result = JSON.parse(response.body, symbolize_names: true)
     expect(response.status).to eq(400)
-    expect(response.body).to_not be_empty
+    expect(result[:error]).to_not be_empty
   end
 
   it "cannot register if duplicate email" do
@@ -48,7 +51,11 @@ describe "users endpoint" do
       "password_confirmation": "password"
       }
 
-    post '/api/v1/users', params: 
+    post '/api/v1/users', params: params
+
+    JSON.parse(response.body, symbolize_names: true)
+    expect(response).to be_successful
+    expect(response.status).to eq(201)
     
     new_params = {
       "email": "example@example.com",
@@ -58,8 +65,9 @@ describe "users endpoint" do
 
     post '/api/v1/users', params: new_params
 
+    result = JSON.parse(response.body, symbolize_names: true)
     expect(response.status).to eq(400)
-    expect(response.body).to_not be_empty
+    expect(result[:error]).to_not be_empty
   end
 
   it "sends an api-key in response body when successful" do
