869402009d350c97ffd6a9af813f1c390afd0934
Tyla Devon <tyladevon@gmail.com>
2020-03-03 11:05:24 -0700

    Remove unnecessary vcr

--- spec/requests/api/v1/can_register_via_post_request_spec.rb
+++ spec/requests/api/v1/can_register_via_post_request_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe "users endpoint" do
-  it "can register with email and password", :vcr do
+  it "can register with email and password" do
     params = {
       "email": "whatever@example.com",
       "password": "password",
@@ -15,7 +15,7 @@ describe "users endpoint" do
     JSON.parse(response.body, symbolize_names: true)
   end
 
-  it "cannot register if passwords are mismatched", :vcr do
+  it "cannot register if passwords are mismatched" do
      params = {
       "email": "whatever@example.com",
       "password": "password",
@@ -39,5 +39,41 @@ describe "users endpoint" do
 
     expect(response.status).to eq(400)
     expect(response.body).to_not be_empty
+  end
+
+  it "cannot register if duplicate email" do
+     params = {
+      "email": "example@example.com",
+      "password": "password",
+      "password_confirmation": "password"
+      }
+
+    post '/api/v1/users', params: 
+    
+    new_params = {
+      "email": "example@example.com",
+      "password": "password",
+      "password_confirmation": "password"
+      }
+
+    post '/api/v1/users', params: new_params
+
+    expect(response.status).to eq(400)
+    expect(response.body).to_not be_empty
+  end
+
+  it "sends an api-key in response body when successful" do
+      params = {
+      "email": "example@example.com",
+      "password": "password",
+      "password_confirmation": "password"
+      }
+
+    post '/api/v1/users', params: params
+
+    result = JSON.parse(response.body, symbolize_names: true)
+    
+    expect(response.status).to eq(201)
+    expect(result[:data][:attributes][:api_key]).to_not be_empty
   end
 end
