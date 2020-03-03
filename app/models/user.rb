b9bad8daae281b99ad75d4b2cf9e98924d46e001
Tyla Devon <tyladevon@gmail.com>
2020-03-02 21:24:30 -0700

    Add validations for password and api_key

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   validates :email, uniqueness: true,  presence: true
-
+  validates_presence_of :password, require: true
   has_secure_password
-  # before :api key method
+  has_secure_token :api_key
 end
