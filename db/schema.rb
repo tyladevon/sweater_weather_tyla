197acbd4c6ecfebba767fff22810f21882c5c3d1
Tyla Devon <tyladevon@gmail.com>
2020-03-02 21:23:23 -0700

    Refactor Users table to include api_key

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_02_233113) do
+ActiveRecord::Schema.define(version: 2020_03_03_041301) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,9 +18,9 @@ ActiveRecord::Schema.define(version: 2020_03_02_233113) do
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password_digest"
-    t.string "status", default: "inactive"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "api_key"
   end
 
 end
