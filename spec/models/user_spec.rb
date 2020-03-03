8623133e16a474aaff0c4bc895b90f735e743466
Tyla Devon <tyladevon@gmail.com>
2020-03-02 21:23:01 -0700

    Add validations to User model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 describe User, type: :model do
   describe 'validations' do
     it {should validate_presence_of(:email)}
-    it {should validate_presence_of(:password)}
+    it {should validate_uniqueness_of(:email)}
+    it {should have_secure_password}
+    it {should have_secure_token(:api_key)}
   end
 end
