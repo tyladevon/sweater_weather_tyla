c56ccd1fd812ad02a394eda288957709dee16fe0
Tyla Devon <tyladevon@gmail.com>
2020-03-03 17:32:04 -0700

    Create Roadtrip controller

---
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -0,0 +1,11 @@
+class Api::V1::RoadTripController < ApplicationController 
+  def create
+    if User.find_by(api_key: params[:api_key])
+      road_trip = RoadTripFacade.new(params[:origin], params[:destination]).roadtrip 
+      binding.pry
+      render json: RoadTripSerializer.new(road_trip), status: 201
+    else
+      render json: {error: "bad credentials"}, status: 401 
+    end
+  end
+end
