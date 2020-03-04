099ad5b0572bd71cdf0ef80ad1b7a166696817f7
Tyla Devon <tyladevon@gmail.com>
2020-03-03 17:28:55 -0700

    Refactor out unecessary variables

--- spec/requests/api/v1/can_login_spec.rb
+++ spec/requests/api/v1/can_login_spec.rb
@@ -3,17 +3,15 @@ require 'rails_helper'
 describe "client can visit sessions endpoint" do 
   describe "can pass email and password through params" do 
     it "can get a 200 ok response with api key" do
-      user = User.create!(
-              email: "whatever@example.com",
-              password: "password",
-              password_confirmation: "password"
-              )
-
+      User.create!(
+        email: "whatever@example.com",
+        password: "password",
+        password_confirmation: "password"
+        )
       params = {
                 "email": "whatever@example.com",
                 "password": "password"
                 }
-
       post '/api/v1/sessions', params: params
 
       expect(response).to be_successful
@@ -21,38 +19,36 @@ describe "client can visit sessions endpoint" do
       expect(result[:data][:attributes][:api_key]).to_not be_empty
     end
     it "gets an error code and message if password is incorrect" do
-      user = User.create!(
-              email: "whatever@example.com",
-              password: "password",
-              password_confirmation: "password"
-              )
+      User.create!(
+        email: "whatever@example.com",
+        password: "password",
+        password_confirmation: "password"
+        )
       params = {
                 "email": "whatever@example.com",
                 "password": "wrong password"
                 }
-
       post '/api/v1/sessions', params: params
 
       result = JSON.parse(response.body, symbolize_names: true)
-      expect(response.status).to eq(400)
+      expect(response.status).to eq(401)
       expect(result[:error]).to_not be_empty
     end
 
     it "gets an error code and message if email is empty" do
-      user = User.create!(
-              email: "whatever@example.com",
-              password: "password",
-              password_confirmation: "password"
-              )
+      User.create!(
+        email: "whatever@example.com",
+        password: "password",
+        password_confirmation: "password"
+        )
       params = {
                 "email": "",
                 "password": "password"
                 }
-
       post '/api/v1/sessions', params: params
 
       result = JSON.parse(response.body, symbolize_names: true)
-      expect(response.status).to eq(400)
+      expect(response.status).to eq(401)
       expect(result[:error]).to_not be_empty
     end
   end
