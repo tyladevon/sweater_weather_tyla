0afe8a99465febe08ad4d99615ed60d00836b742
Tyla Devon <tyladevon@gmail.com>
2020-03-04 10:30:38 -0700

    Add sad path testing for roadtrip. Add updated vcr  recordings.

--- spec/requests/api/v1/can_get_roadtrip_spec.rb
+++ spec/requests/api/v1/can_get_roadtrip_spec.rb
@@ -24,5 +24,25 @@ describe "registered client can post to roadtrip endpoint" do
       expect(result[:data][:attributes][:travel_time]).to_not be_empty
       expect(result[:data][:attributes][:arrival_forecast]).to_not be_empty
     end 
+
+    it "can get an error code and message if post params are incorrect" do
+      user = User.create!(
+                email: "new@example.com",
+                password: "password",
+                password_confirmation: "password"
+                )
+
+      params = {
+                "origin": "Denver,CO",
+                "destination": "Pueblo,CO",
+                "api_key": 1324876089761324
+                }
+
+      post '/api/v1/road_trip', params: params
+
+      result = JSON.parse(response.body, symbolize_names: true)
+      expect(response.status).to eq(401)
+      expect(result[:error]).to_not be_empty
+    end
   end 
 end 
